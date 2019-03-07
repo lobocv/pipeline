05a667a768b70abf4edcde46bf49acb30e33d35b
Calvin Lobo <calvin.lobo@stratoscale.com>
2019-03-07 08:42:57 -0500

    Added dockerfollow, dockerlog, dockerinspect, dockerkill, dockerip, dockeriplist, dockeriface, containernames and retry functions

--- aliases.sh
+++ aliases.sh
@@ -26,24 +26,92 @@ alias gl="git log"
 alias gs="git status"
 
 ########### DOCKER ###############
-function dockerssh() {
-	container=( `docker ps --format "{{.Names}}" | grep $1` )
-	num=${#container}
+
+alias containernames="docker ps --format "{{.Names}}""
+
+function findcontainer() {
+	if [[ -z "$1" ]]; then
+		containers=( `docker ps --format "{{.Names}}"` )
+	else
+		containers=( `docker ps --format "{{.Names}}" | grep $1` )
+	fi
+	num=${#containers}
 	if [[ $num -gt 1 ]]; then
 		echo "More than one live container found for grep \"$1\". Choose a container from below:"
 		lc=1
-		for x in $container; 
+		for x in $containers; 
 		do 
 			echo $lc. $x
 			lc=$((lc+1))
 	       	done
 		read c
-		target=$container[$(($c))]
+		container=$containers[$(($c))]
 	else
-		target=$container[1]
+		container=$containers[1]
 	fi
-	
-	docker exec -it $target ${2:-bash}
+}
+
+# SSH into a running docker container.
+# $1: Container name to grep for
+# $2: Shell to use (default: bash)
+function dockerssh() {
+	findcontainer "$1"
+	docker exec -it "$container" ${2:-bash}
+}
+
+# Follow logs of a running docker container.
+# $1: Container name to grep for
+function dockerfollow() {
+	findcontainer "$1"
+	docker logs -f "$container"
+}
+
+# Dump logs of a running docker container.
+# $1: Container name to grep for
+# $2: Output file path
+function dockerlog() {
+	findcontainer "$1"
+	docker logs "$container"
+}
+
+# Kill a running docker container.
+# $1: Container name to grep for
+function dockerkill() {
+	findcontainer "$1"
+	docker kill $2 "$container"
+}
+
+# Get the info of a running docker container.
+# $1: Container name to grep for
+function dockerinspect() {
+	findcontainer "$1"
+	docker inspect "$container"
+}
+
+
+# Get the IP of a running docker container.
+# $1: Container name to grep for
+function dockerip() {
+	findcontainer "$1"
+	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{printf "\n"}}{{end}}' "$container"
+}
+
+
+# Get the host virtual network interface of the network interface of a running docker container.
+# $1: Container name to grep for
+# $2: container network interface (Default: eth0)
+function dockeriface() {
+	findcontainer "$1"
+	iface=${2:-eth0}
+	ifindex=$(docker exec -it $container sh -c "cat /sys/class/net/${iface}/iflink" | tr -d '\r')
+	iface_path_host=$(grep -R "${ifindex}" /sys/class/net/*/ifindex)
+	echo "$(basename $(dirname $iface_path_host))"
+}
+
+# List the IPs of all running docker container.
+# $1: Container name to grep for
+function dockeriplist() {
+	docker ps -q | xargs -n 1 docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}} {{end}} {{ .Name }}' | sed 's/ \// /'
 }
 
 function dockerrun() {
@@ -62,10 +130,34 @@ function dockerrun() {
 		target=$images[1]
 	fi
 	
-	docker run --rm -it $target ${2:-bash}
+	docker run --rm -it --entrypoint ${2:-bash} $target
 }
 
 
 ########### TOOLS ###############
 
+# Formats stdin to pretty JSON
 alias jsonp="python -m json.tool"
+
+# Retry a command repeatedly until it exits with status code 0
+function retry() {
+	ec="1"
+	count=0
+	cmd=${@:1}
+	out=$(mktemp /tmp/retry.XXXXXX)
+	while : ; do
+		eval $cmd 2> "$out"
+		ec="$?"
+		result=$(cat "$out")
+		# Only echo output if it is different
+		if [[ "$result" != "$prev_result" ]]; then
+			echo $result
+		fi
+		if [[ "$ec" = "0" ]]; then
+			break
+		fi
+		echo -n "."
+		sleep 5
+		prev_result=$result
+	done
+}
