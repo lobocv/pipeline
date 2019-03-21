505476839838dc24228f80d062657b19209eb4ad
Calvin Lobo <calvin.lobo@stratoscale.com>
2019-03-21 09:55:27 -0400

    Added gitrebase()

--- aliases/git.sh
+++ aliases/git.sh
@@ -4,3 +4,14 @@ alias gitch="git checkout"
 alias gd="git diff"
 alias gl="git log"
 alias gs="git status"
+
+# Rebase the current branch onto the most up to date specified branch
+# $1: Branch to rebase onto (Default: master)
+function gitrebase() {
+	set -e
+	base=${1:-master}
+	echo git checkout "$base"
+	echo git pull
+	echo git checkout -
+	echo git rebase "$base"
+}
