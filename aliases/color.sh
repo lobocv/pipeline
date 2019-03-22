fe3f3fbc8578b3f7a96812272789e9c7747ddbf2
Calvin Lobo <calvin.lobo@stratoscale.com>
2019-03-22 10:27:42 -0400

    Added color.sh to help color terminal output

---
+++ aliases/color.sh
@@ -0,0 +1,40 @@
+# Tools for coloring terminal output
+
+DEFAULT_HL_COLOR=93
+# Highlight text
+function _highlight() {
+	CODE=$(printf "\033[%qm" "$1")
+	RESET=$(printf "\033[%qm" "$2")
+	MATCH="$3"
+
+	while read TEXT;
+	do
+		# & acts as the match group in sed
+		echo "$TEXT" | sed -r "s|"$MATCH"|${CODE}&${RESET}|g"
+	done
+}
+
+alias hl="_highlight $DEFAULT_HL_COLOR 39"
+
+# Colors
+alias cBlack="_highlight 30 39"
+alias cRed="_highlight 31 39"
+alias cBlue="_highlight 34 39"
+alias cGreen="_highlight 32 39"
+alias cPink="_highlight 95 39"
+alias cBrown="_highlight 33 39"
+alias cMag="_highlight 35 39"
+alias cWhite="_highlight 0 39"
+alias cCyan="_highlight 36 39"
+alias cGrey="_highlight 90 39"
+alias cYellow="_highlight 93 39"
+
+# Styles
+alias cBold="_highlight 1 21"
+alias cItalic="_highlight 3 24"
+alias cUnderline="_highlight 4 24"
+alias cBlink="_highlight 5 25"
+alias cHighlight="_highlight 7 27"
+alias cHide="_highlight 8 28"
+alias cStrike="_highlight 9 0"
+
