130430ad830ca81d51ca3b2acda6fd897a89fe29
Calvin Lobo <calvin.lobo@stratoscale.com>
2019-03-26 11:09:10 -0400

    Added hold() and release()

--- aliases/filesystem.sh
+++ aliases/filesystem.sh
@@ -36,4 +36,49 @@ function search() {
         find . -iname "*$1*"
 }
 
+# Capture a filepath into a buffer that can be used with other commands (drop, etc) 
+function hold() {
+	if [ -f "$1" ]; then
+		export HOLD_BUFFER="$(readlink -e $1)"
+		echo "Holding $HOLD_BUFFER" 1>&2 
+	else
+		echo "Hold failed: path $1 does not exist" 1>&2 
+		return "1"
+	fi
+}
+
+function whatsheld() {
+	echo "$HOLD_BUFFER"
+}
+function droppath() {
+	export HOLD_BUFFER=""
+}
+
+# Executes a command providing the hold buffer as the first argument
+# Example1: release cp myfile.txt
+# Example2: release chmod +x
+# $1: Command to run
+# $@: Additional arguments or flags to the command called after the hold buffer
+function release() {
+	cmd="$1"
+	shift
+	FLAGS=()
+	ARGS=()
+	while [ ! -z "$1" ];
+	do
+		if [[ "$1" == [-+]* ]]; then
+			FLAGS+=$1
+		else
+			ARGS+=$1
+		fi
+		shift
+	done
+	
+	if [ ! -z "$HOLD_BUFFER" ]; then
+		$cmd $FLAGS "$HOLD_BUFFER" $ARGS
+	else
+		echo "Hold buffer is empty" 1>&2
+		return 1
+	fi
+}
 
